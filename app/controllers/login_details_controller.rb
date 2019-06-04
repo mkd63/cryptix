2e7dab8e4e9c656b8fe03261ba1cfef734a4a1d9
techyon7 <sparshtyagi21@gmail.com>
2019-06-04 14:29:52 +0200

    Fixed login controller

--- app/controllers/login_details_controller.rb
+++ app/controllers/login_details_controller.rb
@@ -2,14 +2,15 @@ class LoginDetailsController < ApplicationController
   def new
   end
   def create
-    user = UserDetail.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    user = UserDetail.find_by(email: params[:login_detail][:email].downcase)
+    logger.debug "The user is #{@user}, the email given is #{params[:login_detail][:email].downcase}"
+    if user #&& user.authenticate(params[:login_detail][:password])
       session[:user_id] = user.id
       flash[:success] = 'Successfully Logged In!'
-      redirect_to '/cryptix'  
+      redirect_to '/cryptix'
     else
       flash[:warning] = "Invalid Username or Password"
-      redirect_to '/register'
+      redirect_to '/login'
     end
   end
   def profile
