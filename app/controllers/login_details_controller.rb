9053f7c0ce3d7804dd75eb986d993764f7878b1c
mkd63 <42798903+mkd63@users.noreply.github.com>
2019-06-04 16:51:17 +0530

    profil, login added

--- app/controllers/login_details_controller.rb
+++ app/controllers/login_details_controller.rb
@@ -2,7 +2,19 @@ class LoginDetailsController < ApplicationController
   def new
   end
   def create
+    user = UserDetail.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully Logged In!'
+      redirect_to '/cryptix'  
+    else
+      flash[:warning] = "Invalid Username or Password"
+      redirect_to '/register'
+    end
+  end
+  def profile
+
   end
   def destroy
-  end 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get    '/login',   to: 'login_details#new'
   post   '/login',   to: 'login_details#create'
+  get    '/cryptix', to: 'login_details#profile'
   delete '/logout',  to: 'login_details#destroy'
+  get    '/register',to: 'user_details#reg'
   resources :user_details do
    get :reg, on: :collection
   end
