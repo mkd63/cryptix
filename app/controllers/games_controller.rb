6742b6645a500cb1eda9ba20bcdcbea58e945090
techyon7 <sparshtyagi21@gmail.com>
2019-06-11 00:44:00 +0200

    Cleaned code for games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,27 +1,25 @@
 class GamesController < ApplicationController
+  $user
+  $level
   def play
     if(session[:user_id] != nil)
       logger.debug "The level is #{session[:user_id]}"
-      user = UserDetail.find(session[:user_id])
-      logger.debug "The level is #{user}"
-      @level = user.level
-      @question = Question.find_by(level: @level)
+      $user = UserDetail.find(session[:user_id])
+      logger.debug "The level is #{$user}"
+      $level = $user.level
+      @question = Question.find_by(level: $level)
     else
       redirect_to '/login'
     end
   end
   def submit_answer
-    logger.debug "The level is #{session[:user_id]}"
-    user = UserDetail.find(session[:user_id])
-    logger.debug "The level is #{user}"
-    @level = user.level
-    @question = Question.find_by(level: @level)
-    @answer = @question.answer
+    @question = Question.find_by(level: $level)
+    answer = @question.answer
     logger.debug "The answer given is #{params[:game][:answer]}"
-    if(params[:game][:answer] == @answer)
+    if(params[:game][:answer] == answer)
       logger.debug "The answer is correct"
-      user.level += 1
-      user.save
+      $user.level += 1
+      $user.save
     else
       logger.debug "The answer is incorrect"
     end
