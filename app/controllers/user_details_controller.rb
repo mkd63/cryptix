8d2e7d4be26147ef4e2c5d9b7221d8ce6b01432e
techyon7 <sparshtyagi21@gmail.com>
2019-06-13 10:07:56 +0200

    Changed login route to root and added link_to to topnav

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -3,15 +3,19 @@ class UserDetailsController < ApplicationController
 
   end
   def show
-    logger.debug "The user is #{session[:user_id]}"
-    @user_detail = UserDetail.find(session[:user_id])
+    if(session[:user_id] != nil)
+      logger.debug "The user is #{session[:user_id]}"
+      @user_detail = UserDetail.find(session[:user_id])
+    else
+      redirect_to '/'
+    end
   end
   def create
     @user_detail = UserDetail.new(user_detail_params)
     @user_detail.level = 0
     @user_detail.last_answered = "2019-02-21 00:00:00"
     @user_detail.save
-    redirect_to '/login'
+    redirect_to '/'
   end
   def user_detail_params
     params.require(:user_detail).permit(:name, :email, :username, :contactno, :password)
